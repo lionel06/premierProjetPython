60f4e8cb0d4bae563b6bcbce31a1610d674dc7db
Lionel Fillatre <Lionel@hp-i3s-fillatre.(none)>
2016-02-20 17:16:23 +0100

    Ajout code Python dans fichier main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-ceci pourrait être du python
+from pandas import Series, DataFrame
+
+try:
+    import numpy as np
+except ImportError:
+    print("numpy is not installed")
+    
+import pandas as pd
+import matplotlib.pyplot as plt
+
+arr = np.arange(10)
+
+obj = Series([4, 7, -5, 3])
